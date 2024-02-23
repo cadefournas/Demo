a49343cdc3b0160a4bc89393ff4227f2107cea77
Carle0009 <cadefournas@gmail.com>
2024-02-23 22:44:07 +0100

    Initial commit

---
+++ ggit.c
@@ -0,0 +1,9 @@
+//
+//  ggit.c
+//  
+//
+//  Created by Charles-antoine de fournas on 23.02.24.
+//
+
+#include "ggit.h"
+int a;
---
+++ ggit.h
@@ -0,0 +1,13 @@
+//
+//  ggit.h
+//  
+//
+//  Created by Charles-antoine de fournas on 23.02.24.
+//
+
+#ifndef ggit_h
+#define ggit_h
+
+#include <stdio.h>
+
+#endif /* ggit_h */
